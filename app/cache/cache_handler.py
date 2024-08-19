430f51930a64f49d0e87a9c9ff0aa269336afc69
Augusto Pareja <augustopareja@gethitch.ai>
2024-08-19 10:51:21 -0300

    Refactor and migration from in-memory-cache to redis

--- app/cache/cache_handler.py
+++ app/cache/cache_handler.py
@@ -1,7 +1,33 @@
 import os
-from cachetools import TTLCache
+import redis
+import json
 
-CACHE_TTL = int(os.getenv("CACHE_TTL"))
-CACHE_MAXSIZE = int(os.getenv("CACHE_MAXSIZE"))
+class RedisCacheHandler:
+    def __init__(self):
+        self.host = os.getenv("REDIS_HOSTNAME")
+        self.port = int(os.getenv("REDIS_PORT"))
+        self.password = os.getenv("REDIS_PASSWORD")
+        self.ttl = int(os.getenv("CACHE_TTL"))
 
-cache = TTLCache(maxsize=CACHE_MAXSIZE, ttl=CACHE_TTL)
+        self.client = redis.StrictRedis(
+            host=self.host,
+            port=self.port,
+            password=self.password,
+            decode_responses=True
+        )
+
+    def set(self, key, value):
+        """Set serialized value in Redis with a TTL."""
+        serialized_value = json.dumps(value)
+        self.client.setex(key, self.ttl, serialized_value)
+
+    def get(self, key):
+        """Get and deserialize value from Redis by key."""
+        data = self.client.get(key)
+        if data:
+            return json.loads(data)
+        return None
+
+    def clear(self):
+        """Clear all data in Redis."""
+        self.client.flushdb()
--- app/services/berry_service.py
+++ app/services/berry_service.py
@@ -1,15 +1,18 @@
 import logging
 import statistics
 from app.data_providers.pokeapi_provider import fetch_all_berries
-from app.cache.cache_handler import cache
+from app.cache.cache_handler import RedisCacheHandler
 
+cache = RedisCacheHandler()
 logging.basicConfig(level=logging.INFO)
 
 async def get_berry_stats():
-    if 'berry_stats' in cache:
-        logging.info("Returning from cache")
-        return cache['berry_stats']
+    cached_data = cache.get("berry_stats")
+    if cached_data:
+        logging.info("Fetching data from cache.")
+        return cached_data
     
+    logging.info("Fetching data from PokeAPI.")
     berries = await fetch_all_berries()
     growth_times = [berry['growth_time'] for berry in berries if 'growth_time' in berry]
     
@@ -20,8 +23,9 @@ async def get_berry_stats():
         "max_growth_time": int(max(growth_times)) if growth_times else None,
         "variance_growth_time": float(statistics.variance(growth_times)) if len(growth_times) > 1 else None,
         "mean_growth_time": float(statistics.mean(growth_times)) if growth_times else None,
-        "frequency_growth_time": {int(x): int(growth_times.count(x)) for x in set(growth_times)}
+        "frequency_growth_time": {str(x): int(growth_times.count(x)) for x in set(growth_times)}
     }
 
-    cache['berry_stats'] = stats
+    cache.set("berry_stats", stats)
+    logging.info("Storing data in cache.")
     return stats
